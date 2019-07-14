d81b482e900ee9cf1ca8abcda71558dc757b80d9
Andrew Zigler <andrewzigler@gmail.com>
2019-07-13 17:09:41 -0700

    Properly handle logout to deauthenticate user on all hosts

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,9 +4,7 @@
     <v-toolbar v-if="$auth.loggedIn" app>
       <v-btn class="ml-3" variant="info" to="/">Home</v-btn>
       <v-btn class="ml-3" variant="info" to="/admin">Admin</v-btn>
-      <v-btn class="ml-3" variant="danger" @click="$auth.logout()"
-        >Logout</v-btn
-      >
+      <v-btn class="ml-3" variant="danger" @click="logout">Logout</v-btn>
       <div>hi:{{ given_name }}</div>
     </v-toolbar>
     <v-content>
@@ -32,6 +30,13 @@ export default {
       return app.$auth.user ? app.$auth.user.given_name : ''
     }
   },
+
+  methods: {
+    logout() {
+      this.$auth.logout()
+      window.location.href = `https://mypetadoption.auth0.com/v2/logout?returnTo=${location.protocol.substr(0, location.protocol.length - 1)}%3A%2F%2F${location.host}`
+    }
+  }
 }
 </script>
 
